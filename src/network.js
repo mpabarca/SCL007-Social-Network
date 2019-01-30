5697856a0e210d73e156631bbffaceee07544f93
AliceInWonderdev <ali.castaneda05@gmail.com>
2019-01-30 12:50:53 -0300

    Changing FireBase constante from main.js to network.js

---
+++ src/network.js
@@ -0,0 +1,12 @@
+(function(){
+    const config = {
+        apiKey: "AIzaSyDzLbBDkog-28DBB1ykLhIzzqdbhGRsb_E",
+        authDomain: "redsocial-e2ac6.firebaseapp.com",
+        databaseURL: "https://redsocial-e2ac6.firebaseio.com",
+        projectId: "redsocial-e2ac6",
+        storageBucket: "redsocial-e2ac6.appspot.com",
+        messagingSenderId: "250121361452"
+      };
+      firebase.initializeApp(config);
+    
+    }());
