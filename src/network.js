ae0a38a14d187d11fa65e17e31d61680af43ff64
AliceInWonderdev <ali.castaneda05@gmail.com>
2019-02-01 16:28:59 -0300

    Firebase:basic structure

--- src/network.js
+++ src/network.js
@@ -1,12 +1,70 @@
-(function(){
-    const config = {
-        apiKey: "AIzaSyDzLbBDkog-28DBB1ykLhIzzqdbhGRsb_E",
-        authDomain: "redsocial-e2ac6.firebaseapp.com",
-        databaseURL: "https://redsocial-e2ac6.firebaseio.com",
-        projectId: "redsocial-e2ac6",
-        storageBucket: "redsocial-e2ac6.appspot.com",
-        messagingSenderId: "250121361452"
-      };
-      firebase.initializeApp(config);
-    
-    }());
+function register(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+       
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        
+        // ...
+      });
+}
+
+function entry(){
+    const email2 = document.getElementById("email2").value;
+    const password2 = document.getElementById("password2").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email2, password2).catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+function viewer(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("si existe usuario")
+            aparece()
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          console.log("no existe usuario")
+          // ...
+        }
+      });
+}
+viewer();
+
+function aparece(){
+    const container = document.getElementById("container");
+    container.innerHTML= `
+    <p>Bienvenido</p>
+    <button onclick="close()">Cerrar Sesión</button>
+    `;
+}
+function close(){
+    firebase.auth().singOut()
+    .then(function(){
+        console.log(saliendo)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
