0e0d43c3c84514a6e13d24590535f605df9db75b
mpabarca <mpabarca@uc.cl>
2019-02-03 14:38:36 -0300

    se arregla comas y llaves network.spec.js

--- test/network.spec.js
+++ test/network.spec.js
@@ -6,12 +6,12 @@ describe('validateEmail', () => {
     it('Debería ser una función', () => {
         assert.equal(typeof validateEmail, 'function');
       });
-    it('Si ingreso name@gmail.com debería retornar true', () => {
+    it('Si ingreso name@gmail.com debería retornar true'), () => {
         assert.equal(validateEmail("name@gmail.com"),true);
-    };
+    },
     it('Si ingreso name.com debería retornar false'), ()=> {
         assert.equeal(validateEmail("name.com"),false);
-    })
+    }
 
 });
 
@@ -19,32 +19,32 @@ describe('validatePassword', () => {
     it('Debería ser una función', () => {
         assert.equal(typeof validatePassword, 'function');
       });
-    it('Si ingreso 123456 debería retornar true', () => {
+    it('Si ingreso 123456 debería retornar true'), () => {
         assert.equal(validatePassword("123456"),true);
-    };
+    },
     it('Si ingreso 123 debería retornar false'), ()=> {
         assert.equeal(validatePassword("123"),false);
-    })
+    }
 
 
 });
 
 describe('createUser', () => {
     it('Debería ser una función', () => {
-        assert.equal(typeof createUser', 'function');
+        assert.equal(typeof createUser, 'function');
       });
-    it('Si ingreso name@gmail.com y 123456 debería retornar true', () => {
+    it('Si ingreso name@gmail.com y 123456 debería retornar true'), () => {
         assert.equal(createUser("name@gmail.com","123456"),true);
-    };
+    },
     it('Si ingreso name@gmail.com y 123 debería retornar false'), ()=> {
         assert.equeal(createUser("name@gmail.com","123"),false);
-    });
+    },
     it('Si ingreso gmail.com y 123456 debería retornar false'), ()=> {
         assert.equeal(ccreateUser("gmail.com","123456"),false);
-    });
+    },
     it('Si ingreso gmail.com y 123 debería retornar false'), ()=> {
         assert.equeal(createUser("gmail.com","123"),false);
-    })
+    }
 
 });
 
@@ -52,18 +52,18 @@ describe('verifyUser', () => {
     it('Debería ser una función', () => {
         assert.equal(typeof verifyUser, 'function');
       });
-    it('Si ingreso name@gmail.com y 123456 debería retornar true', () => {
+    it('Si ingreso name@gmail.com y 123456 debería retornar true'), () => {
         assert.equal(verifyUser("name@gmail.com","123456"),true);
-    };
+    },
     it('Si ingreso name@gmail.com y 123 debería retornar false'), ()=> {
         assert.equeal(verifyUser("name@gmail.com","123"),false);
-    });
+    },
     it('Si ingreso gmail.com y 123456 debería retornar false'), ()=> {
         assert.equeal(cverifyUser("gmail.com","123456"),false);
-    });
+    },
     it('Si ingreso gmail.com y 123 debería retornar false'), ()=> {
         assert.equeal(verifyUser("gmail.com","123"),false);
-    })
+    }
 
 
 });
