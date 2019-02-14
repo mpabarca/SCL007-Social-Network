50a8c8fdfbf9f7f7d32b0fa6d16bb8d648eb0e92
AliceInWonderDev <ali.castaneda05@gmail.com>
2019-02-14 16:57:08 -0300

    actualizando

--- src/js/network.js
+++ src/js/network.js
@@ -18,46 +18,50 @@ document.getElementById("registro").addEventListener("click",() => {
       });
 })
 
- //STORAGE GUARDAR DATOS EN FIRE
- window.onload = () => { 
-    firebase.auth().onAuthStateChanged = ( user => {
-    guardar = () => {
-    let tituloPublicacion = document.getElementById("tituloPublicacion").value;
-    let textoPublicacion = document.getElementById("textoPublicacion").value;
-    let fechaPublicacion = new Date();
+//INGRESO USUARIO VIA MAIL Y CLAVE
+document.getElementById("acceder").addEventListener("click",() => {
+    let email2 = document.getElementById('email').value;
+    let contrasena2 = document.getElementById('contrasena').value;
 
-     var db = firebase.firestore(); 
-
-    db.collection("users").doc(user.uid).set({ 
-        email: user.email, 
-        displayName: user.displayName,
-    });
-
-    db.collection('post').add({ //AÑADIENDO EN FIRESTORE COLECCION: "POST"
-        titulo : tituloPublicacion,
-        texto: textoPublicacion,
-        fecha: fechaPublicacion,
-        uid: user.uid,
-        email: user.email, 
-        displayName: user.displayName,
-        comentarios : 0,
-        like: 0, 
-        photo: user.photoURL
-
-    })
-
-    .then(docRef => {
-        document.getElementById("tituloPublicacion").value = ''; //Limpiar
-        document.getElementById("textoPublicacion").value = ''; // Limpiar
-        console.log("Se subio a dataBase correctamente")
+    firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+    .then(function(){        
     })
     .catch(error => {
-        console.error("Error adding document: ", error);
-    });
-    }
-     
-    });
+        // Handle Errors here.
+        if(contrasena2.length <= 5) {
+            alert("Ingrese contraseña de 6 dígitos o más");
+        }else if (email2.indexOf("@"));
+            alert("Ingrese email válido");
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+      });
+})
+
+//OBSERVA SI ES UN USUARIO REGISTRADO
+observador = () => {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            aparece(user);
+          // User is signed in.
+          let displayName = user.displayName;
+          let email = user.email;
+          //console.log(user);
+          let emailVerified = user.emailVerified;
+          console.log(user.emailVerified)
+          let photoURL = user.photoURL;
+          console.log (user.photoURL)
+          let isAnonymous = user.isAnonymous;
+          let uid = user.uid;
+          console.log(user.uid)
+          let providerData = user.providerData;
+          console.log (user.providerData[0].providerId)
+        } else {
+            console.log("No existe usuario activo")
+            //apareceNousuario(); //ingresa tus datos para acceder
+            }
+      });
 }
+observador();
 
 //APARECE INFORMACION SOLO SI EL USUARIO VERIFICA SU CUENTA CON CORREO ENVIADO AL MAIL
 aparece = user => {
@@ -114,6 +118,16 @@ function timeConverter(UNIX_timestamp){
     return time;
   }
 
+//MOSTRAR COLECCION POST CON TITULO Y TEXTO DE LA PUBLICACION
+let contenido2 = document.getElementById('contenido2');
+
+db.collection("post").orderBy("fecha", "desc").limit(10).onSnapshot(querySnapshot => {
+    contenido2.innerHTML = "";
+    querySnapshot.docs.forEach(doc => {
+        
+        //console.log(`uid USUARIO:  ${user.uid}`)// uid del usuario
+        //console.log(`uid de POST:  ${doc.data().uid}`)
+        //console.log("-----------------------------------------------------------")
 
         if (user.uid === doc.data().uid) { //si el id del usuario registrado es igual al uid del post registrado entonces... 
             //console.log ("Se muestre icono borrar")
@@ -176,6 +190,11 @@ function timeConverter(UNIX_timestamp){
                     </li>
                 </ul>`
         }
+    });
+});
+
+
+}
 
 //CERAR SESION USUARIOS LOG
 cerrar = () => {
@@ -288,47 +307,15 @@ guardar = () => {
     .catch(error => {
         console.error("Error adding document: ", error);
     });
-    }
+}
      
     });
-}
 
-//FUNCION PARA CONVERTIR TIMESTRAMP A FECHA HUMANA
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min ;
-    return time;
-  }
-
-
-
-
-//RECUPERAR CONTRASEÑA
-document.getElementById("forgot-pass").addEventListener("click",() => {
-        var auth = firebase.auth();
-        let email = document.getElementById('email').value;
-        alert("Ingresa tu mail para reestablecer")
-    auth.sendPasswordResetEmail(email)
-    .then( () => {
-        alert("Revisa tu correo para cambiar tu contraseña")
-      // Email sent.
-    }).catch(error  => {
-        console.log("No se a enviado mail")
-      // An error happened.
-    });
-})
-
+    
 //BORRAR DATOS
-var eliminar ="";
 eliminar = (id) => {
     var db = firebase.firestore(); 
-    confirm("¿Estas seguro que quieres eliminarlo?")
+    confirm("Estas seguro que quieres eliminarlo?")
     db.collection("post").doc(id).delete()
         .then(() => {
         console.log("Post borrado");
@@ -367,50 +354,6 @@ function editar(id, textoPublicacion, etiquetaPublicacion){
     }    
 }
 
-
-
-/*/CERAR SESION USUARIOS LOG
-
-cerrar = () => {
-    firebase.auth().signOut()
-        console.log('Saliendo...')
-}*/
-
-//LIKES
-var postDivs = document.querySelectorAll('.post');
-
-function getLikeCount(postID) {
-    var thisPostRef = new Firebase(firebaseURL + postID + '/like-count/');
-    thisPostRef.once('value', function(snapshot) {
-        if ( snapshot.val() ) {
-            document.querySelector('#' + postID + ' .like-count').innerHTML = snapshot.val() + ' likes';
-        } else {
-            return false;
-        }
-    });
-}
-for (var i = 0; i < postDivs.length; i++) {
-    var postID = postDivs[i].id;
-    var numLikes = getLikeCount(postID);
-}
-function likePost(uid) {
-    var postRef = new Firebase(firebaseURL + uid);
-    postRef.child('like-count').once('value', function(snapshot) {
-        var currentLikes = snapshot.val() ? snapshot.val() : 0;
-        postRef.update({
-            'postID': id,
-            'like-count': currentLikes + 1
-            }, function(error) {
-              if (error) {
-                console.log('Data could not be saved:' + error);
-              } else {
-                console.log('Data saved successfully');
-              }
-            });
-        getLikeCount(uid);
-    });   
-}
-
 //PROFILE
 document.getElementById("profile").addEventListener("click",() => {
     firebase.auth().onAuthStateChanged(user => {
@@ -443,4 +386,3 @@ profile = user => {
     `
 }
 ;
-
